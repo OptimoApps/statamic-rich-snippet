bd985bc556adbbcc5ab39e4b52f63e6513bff173
satz <sathish.thi@gmail.om>
2020-08-11 18:20:10 +0530

    :white_check_mark: adding test

---
+++ tests/TestCase.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * *
+ *  *  * Copyright (C) OptimoApps - All Rights Reserved
+ *  *  * Unauthorized copying of this file, via any medium is strictly prohibited
+ *  *  * Proprietary and confidential
+ *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com>
+ *  *
+ *
+ */
+
+namespace OptimoApps\RichSnippet\Tests;
+
+use OptimoApps\RichSnippet\ServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Statamic\Extend\Manifest;
+use Statamic\Providers\StatamicServiceProvider;
+use Statamic\Statamic;
+
+class TestCase extends OrchestraTestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            StatamicServiceProvider::class,
+            ServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Statamic' => Statamic::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app->make(Manifest::class)->manifest = [
+            'optimoapps/statamic-rich-snippet' => [
+                'id' => 'optimoapps/statamic-rich-snippet',
+                'namespace' => 'OptimoApps\\RichSnippet\\',
+            ],
+        ];
+    }
+}
