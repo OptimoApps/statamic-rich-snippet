aa4fcedb4751ecd03acec7f0518fd5d82b7aa5cd
ssatz <sathish.thi@gmail.com>
2020-08-30 12:18:30 +0530

    :zap: rename section `Rich Snippet` to `Schema.org`
    Signed-off-by: ssatz <sathish.thi@gmail.com>

--- src/Listeners/AddRichSnippetBluePrint.php
+++ src/Listeners/AddRichSnippetBluePrint.php
@@ -1,10 +1,10 @@
 <?php
 /*
  * *
- *  *  * Copyright (C) OptimoApps - All Rights Reserved
+ *  *  * Copyright (C) optimoapps.com - All Rights Reserved
  *  *  * Unauthorized copying of this file, via any medium is strictly prohibited
  *  *  * Proprietary and confidential
- *  *  * Written by Sathish Kumar(satz) <info@optimoapps.com>
+ *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com>, ManiKandan<smanikandanit@gmail.com >
  *  *
  *
  */
@@ -18,14 +18,14 @@ class AddRichSnippetBluePrint
 {
     public function handle(EntryBlueprintFound $event): void
     {
-        if (! is_null($event->entry)) {
+        if (!is_null($event->entry)) {
             //to enable rich snippet
             $event->blueprint->ensureField('is_rich_snippet', [
                 'type' => 'toggle',
                 'display' => 'Is Rich Snippet?',
                 'default' => 'true',
                 'instructions' => 'Enable / Disable Rich Snippet',
-            ], 'Rich Snippet');
+            ], 'Schema.org');
 
             //select type
             $event->blueprint->ensureField('schema_type', [
@@ -38,28 +38,28 @@ class AddRichSnippetBluePrint
                     SchemaTypeEnum::BLOG_POSTING => 'Blog Posting',
                     SchemaTypeEnum::NEWS_ARTICLE => 'News Article',
                 ],
-            ], 'Rich Snippet');
+            ], 'Schema.org');
 
             /*
              * Article Schema FieldType
              */
             $event->blueprint->ensureField('article_schema', ['type' => 'article_schema', 'listable' => false, 'if' => [
                 'schema_type' => 'equals Article',
-            ]], __('Rich Snippet'));
+            ]], __('Schema.org'));
 
             /*
              *  BlogPosting Schema FieldType
              */
             $event->blueprint->ensureField('blog_schema', ['type' => 'blog_schema', 'listable' => false, 'if' => [
                 'schema_type' => 'equals BlogPosting',
-            ]], __('Rich Snippet'));
+            ]], __('Schema.org'));
 
             /*
              *  NewsArticle Schema FieldType
              */
             $event->blueprint->ensureField('news_schema', ['type' => 'news_schema', 'listable' => false, 'if' => [
                 'schema_type' => 'equals NewsArticle',
-            ]], __('Rich Snippet'));
+            ]], __('Schema.org'));
         }
     }
 }
